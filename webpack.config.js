96e2b6a739085a05890b62939d92bf080b390512
Aibek <aibek.abdykasymov@gmail.com>
2017-04-14 03:16:03 +0600

    num#2.5 Simple config - Environment NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./home",
     output: {
         filename: "bundle.js",
         library: "home"
     },
-    watch: true,
+    watch: NODE_ENV == "development",
     watcherOptions: {
         aggregateTimeout: 100
     },
-    devtool: "cheap-inline-source-map"
+    devtool: NODE_ENV == "development" ? "cheap-inline-module-source-map" : null,
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV),
+            LANG: JSON.stringify('ru'),
+            USER: JSON.stringify(process.env.USER)
+        })
+    ]
 };
--- welcome.js
+++ welcome.js
@@ -1,5 +1,8 @@
 'use strict';
 module.exports=function (message) {
-    debugger;
+    if(NODE_ENV=='development'){
+        console.log(message);
+    }
+    alert(USER);
     alert("welcome "+message);
 };
